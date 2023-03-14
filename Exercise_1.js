4ca3795161aefe3e2acdf42f08f74e515498fd36
Viewls2 <mahasamut.kh@gmail.com>
2023-03-14 17:52:30 +0700

    feat(2): calculate transaction fee.

--- Exercise_1.js
+++ Exercise_1.js
@@ -1,5 +1,10 @@
-const payment1 = [100, 200, 300].reduce((partialSum, a) => partialSum + a, 0);
-console.log(payment1);
+const payment1 = [100, 200, 300] //payment
+const totalPay = payment1.reduce((Sum, n) => Sum + n, 0); // total payment
+console.log(totalPay);
 
-const numPay = payment1.lenght;
-console.log(numPay);
+const numPay = payment1.length*3; //3$ per transaction
+
+const addFee = (totalPay*0.01) +(numPay*0.01); //addition cost
+console.log(addFee);
+const finalPay = addFee + totalPay; //total pay
+console.log(finalPay);
